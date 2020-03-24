0b6ed7021c58b45c0c89ddab2ce4cb1d135dd402
Ivan Golubey <ig2108@yahoo.com>
2020-03-24 20:23:53 +0200

    starting commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const link = React.createElement(
+  "a",
+  {
+    href: "http://reactjs.org",
+    target: "_blank"
+  },
+  "react website"
+);
+
+console.log(link);
+
+ReactDOM.render(link, document.querySelector("#root"));
