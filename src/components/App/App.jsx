d3e3458f86663580d1a4856c1d841e61a64580ef
Ivan Golubey <ig2108@yahoo.com>
2021-01-11 09:59:00 +0200

    added sass

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import styles from './App.module.scss';
+
+export default class App extends Component {
+  state = {  }
+  render() {
+    return (
+      <div className={styles.container}></div>
+    );
+  }
+}
